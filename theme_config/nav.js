c77952529bd427c523564e061ff788a9161205b1
Mr.zhang <85136735@qq.com>
2026-03-31 22:16:36 +0800

    增加内容

--- theme_config/nav.js
+++ theme_config/nav.js
@@ -26,6 +26,24 @@ export const nav = [
     ],
   },
   {
+    "text": "数据库",
+    "link": "/database/",
+    "items": [
+      { "text": "🗄️ MySQL", "link": "/database/mysql/" },
+      { "text": "🔴 Redis", "link": "/database/redis/" },
+      { "text": "🍃 MongoDB", "link": "/database/mongodb/" },
+      { "text": "🟪 TiDB", "link": "/database/tidb/" },
+      { "text": "🔵 达梦（DM）", "link": "/database/dameng/" },
+      { "text": "🐘 PostgreSQL", "link": "/database/postgresql/" },
+      { "text": "🏛️ Oracle", "link": "/database/oracle/" },
+      { "text": "📊 SQL Server", "link": "/database/sqlserver/" },
+      { "text": "📱 SQLite", "link": "/database/sqlite/" },
+      { "text": "🦭 MariaDB", "link": "/database/mariadb/" },
+      { "text": "📈 时序数据库（TSDB）", "link": "/database/tsdb/" },
+      { "text": "🐘 HBase", "link": "/database/hbase/" }
+    ]
+  },
+  {
     "text": "常用框架",
     "link": "/framework/",
     "items": [
@@ -39,12 +57,51 @@ export const nav = [
     ]
   },
   {
+    "text": "分布式架构",
+    "link": "/distributed/",
+    "items": [
+      { "text": "📐 分布式理论", "link": "/distributed/theory/" },
+      { "text": "💰 分布式事务", "link": "/distributed/transaction/" },
+      { "text": "🔒 分布式锁", "link": "/distributed/lock/" },
+      { "text": "📡 分布式协调服务", "link": "/distributed/coordination/" },
+      { "text": "⚖️ 服务治理", "link": "/distributed/governance/" },
+      { "text": "📞 服务调用", "link": "/distributed/rpc/" },
+      { "text": "🗺️ 典型分布式场景", "link": "/distributed/scenario/" }
+    ]
+  },
+  {
     "text": "三高架构",
     "link": "/high-throughput/",
     "items": [
       { "text": "📈 高性能优化", "link": "/high-throughput/high-performance" },
       { "text": "🔥 高并发设计", "link": "/high-throughput/high-concurrency" },
       { "text": "🏆 高可用架构", "link": "/high-throughput/high-availability" }
+    ]
+  },
+  {
+    "text": "中间件",
+    "link": "/middleware/",
+    "items": [
+      { "text": "📨 消息队列", "link": "/middleware/mq/" },
+      { "text": "🔌 RPC 框架", "link": "/middleware/rpc/" },
+      { "text": "📡 注册中心", "link": "/middleware/registry/" },
+      { "text": "🚪 API 网关", "link": "/middleware/gateway/" },
+      { "text": "🔍 搜索引擎", "link": "/middleware/search/" },
+      { "text": "🧩 其他中间件", "link": "/middleware/other/" }
+    ]
+  },
+  {
+    "text": "工程化与运维",
+    "link": "/devops/",
+    "items": [
+      { "text": "🧊 Docker", "link": "/devops/docker/" },
+      { "text": "☸️ Kubernetes", "link": "/devops/k8s/" },
+      { "text": "🔨 构建工具", "link": "/devops/build-tool/" },
+      { "text": "🔁 CI/CD", "link": "/devops/cicd/" },
+      { "text": "🔗 容器编排对比", "link": "/devops/orchestration/" },
+      { "text": "⚙️ 自动化运维", "link": "/devops/ops/" },
+      { "text": "🟦 KubeSphere", "link": "/devops/kubesphere/" },
+      { "text": "🏗️ 基础设施即代码", "link": "/devops/iac/" }
     ]
   }
 ]
--- theme_config/sidebar.js
+++ theme_config/sidebar.js
@@ -1,13 +1,21 @@
 import { csSidebar } from './sidebar/cs'
+import { databaseSidebar } from './sidebar/database'
+import { devopsSidebar } from './sidebar/devops'
+import { distributedSidebar } from './sidebar/distributed'
 import { frameworkSidebar } from './sidebar/framework'
-import { interviewSidebar } from './sidebar/interview'
 import { highThroughputSidebar } from './sidebar/high-throughput'
+import { interviewSidebar } from './sidebar/interview'
 import { javaSidebar } from './sidebar/java'
+import { middlewareSidebar } from './sidebar/middleware'
 
 export const sidebar = {
   '/cs/': csSidebar,
+  '/database/': databaseSidebar,
+  '/devops': devopsSidebar,
+  '/distributed/': distributedSidebar,
   '/framework/': frameworkSidebar,
-  '/interview/': interviewSidebar,
   '/high-throughput/': highThroughputSidebar,
+  '/interview/': interviewSidebar,
   '/java/': javaSidebar,
+  '/middleware/': middlewareSidebar,
 }
