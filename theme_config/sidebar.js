fa43ecbe5a969ce5ac30932d26be800789b4f68e
Mr.zhang <85136735@qq.com>
2026-04-08 23:18:04 +0800

    调整主题色

--- theme_config/sidebar.js
+++ theme_config/sidebar.js
@@ -1,7 +1,19 @@
 import { csSidebar } from './sidebar/cs'
-
+import { javaSidebar } from './sidebar/java'
+import { databaseSidebar } from './sidebar/database'
+import { frameworkSidebar } from './sidebar/framework'
+import { distributedSidebar } from './sidebar/distributed'
+import { designSidebar } from './sidebar/design'
+import { questionsSidebar } from './sidebar/questions'
+import { interviewPrepSidebar } from './sidebar/interview-prep'
 
 export const sidebar = {
+  '/interview-prep/': interviewPrepSidebar,
   '/cs/': csSidebar,
-
+  '/java/': javaSidebar,
+  '/database/': databaseSidebar,
+  '/framework/': frameworkSidebar,
+  '/distributed/': distributedSidebar,
+  '/design/': designSidebar,
+  '/questions/': questionsSidebar,
 }
